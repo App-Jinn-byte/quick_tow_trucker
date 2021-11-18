ef7d28a019162347233557d374a2cec690e54783
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-11-18 15:25:52 +0500

    UI changes implemented with trello cards

--- lib/screens/auth/login_screens/login_screen.dart
+++ lib/screens/auth/login_screens/login_screen.dart
@@ -82,7 +82,6 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     loginProvider = Provider.of<LoginProvider>(context, listen: true);
-
     return SafeArea(
         child: Scaffold(
       resizeToAvoidBottomInset: false,
